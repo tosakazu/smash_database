e0f6604f302fdbe56b971847880b5cbb41f82506
tosakazu <162583501+tosakazu@users.noreply.github.com>
2024-12-01 05:47:51 +0900

    Update download.py

--- scripts/download.py
+++ scripts/download.py
@@ -373,7 +373,7 @@ def fetch_latest_tournaments_by_game(game_id, jp_only, limit=5, page=1):
         get_tournaments_by_game_query(jp_only),
         {"gameId": game_id, "perPage": limit, "page": page},
     )
-    if "data" in response_data and response_data["data"] is not None and "tournament" not in response_data["data"]:
+    if "data" in response_data and response_data["data"] is not None and "tournaments" not in response_data["data"]:
         raise FetchError(f"Error: 'data' or 'tournament' key not found in response for game {game_id}. Response data: {response_data}\n in fetch_latest_tournaments_by_game")
         
     tournaments = response_data["data"]["tournaments"]["nodes"]
